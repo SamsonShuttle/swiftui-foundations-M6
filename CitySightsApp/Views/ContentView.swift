e6ba09de54b31a30ab2a690ff98eb440c6c3d4ac
SamsonShuttle <samsonshuttle@sky.com>
2022-03-07 12:19:52 +0000

    - Set up folders for Views, ViewModels, Models - Renamed the ContentView to LaunchView

--- CitySightsApp/Views/ContentView.swift
+++ CitySightsApp/Views/ContentView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  LaunchView.swift
 //  CitySightsApp
 //
 //  Created by Samson Shuttle on 07/03/2022.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LaunchView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
@@ -16,6 +16,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LaunchView()
     }
 }
